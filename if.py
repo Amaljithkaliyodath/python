bae455f2a9c1f5905bfbf7a07a9cff2829ace83e
amaljithkaliyodath <amaljithkaliyodath1@gmail.com>
2023-09-28 15:52:27 +0530

    add

--- if.py
+++ if.py
@@ -62,18 +62,97 @@
 
 
 
-a=int(input('enter your age'))
+# a=int(input('enter your age'))
 
-if a<=2:
-   print(a,"your are infant")
-elif a<=12:
-    print(a,"your child")
-elif a<=19:
-    print(a,"your teenager")
-elif a<=64:
-    print(a,"your adult")
-elif a>=65:
-    print(a,"your senior")
+# if a<=2:
+#    print(a,"your are infant")
+# elif a<=12:
+#     print(a,"your child")
+# elif a<=19:
+#     print(a,"your teenager")
+# elif a<=64:
+#     print(a,"your adult")
+# elif a>=65:
+#     print(a,"your senior")
+
+
+  
+
+# a=int(input('enter a number'))
+# b=int(input('enter a number'))
+# c=int(input('enter a number'))
+# if a>b and a>c:
+#     print('a is larger')
+# elif b>a and b>c:
+#     print('b is larger')
+# elif c>a and c>b:
+#     print('c is larger')
+# else:
+#     print('both are equal')
 
 
 
+
+# a=int(input('enter a number'))
+# if a==1:
+#     print('sunday')
+# elif a==2:
+#     print('monday')
+# elif a==3:
+#     print('tuesday')    
+# elif a==4:
+#     print('wednesday')
+# elif a==5:
+#     print('thursday')   
+# elif a==6:
+#     print('friday')
+# elif a==7:
+#     print('saturday')
+
+
+
+
+# w=int(input('enter weigth'))
+# h=int(input('enter a heigth'))
+# b=w/h
+# if b<=18.5:
+#     print('underweigth')
+# elif b<=24.9:
+#     print('normal weigth')
+# elif b<=29.9:
+#     print('overwweigth')
+# else:
+#     print('obesity')
+
+
+# a=str((input('enter color')))
+# b='red'
+# c='green'
+# d='yellow'
+# if a in c:
+#     print('go')
+# elif a in b:
+#     print('stop') 
+# elif a in d:
+#   print('proceed with caution')
+
+
+# a=int(input('enter a time '))
+# if a>=5 and a<=11:
+#     print('good morning')
+# elif a>=12 and a<=16:
+#     print('good afternoon')
+# elif a>=17 and a<=20:
+#     print('good evening')
+# elif a>=21 and a<=24:
+#     print('good night')
+# elif a<=4:
+#     print('good night')
+
+a=str(input('enter a character'))
+print(a.isdigit())
+print(a.isalpha())
+if (a.upper()):
+  print(a,'is uppercase')
+elif (a.lower()):
+  print(a,'is lowercase')
