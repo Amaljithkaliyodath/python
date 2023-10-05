025ba8bdc1be30f7bb2cf3c0a05cc8b07fbe0fb3
amaljithkaliyodath <amaljithkaliyodath1@gmail.com>
2023-10-05 15:57:55 +0530

    add

--- for.py
+++ for.py
@@ -16,19 +16,39 @@
 
 
 
-a="abababababab"
-c=0
-for i in a:
-  if i=='a':
-    c=c+1
-    if i=='b':
-      c=c-1
-    elif c==0:
-      print('balanced')
-    else:
-      print("not balanced")
+# a="abababababab"
+# c=0
+# for i in a:
+#   if i=='a':
+#     c=c+1
+#     if i=='b':
+#       c=c-1
+#     elif c==0:
+#       print('balanced')
+#     else:
+#       print("not balanced")
 
 
-
-
- 
+a=[2,3,4,1,5]
+tar=6
+b=[]
+# if a[0]+a[1]==tar:
+#     b.append(0)
+#     b.append(1)
+#     print(b)
+# elif a[1]+a[2]==tar:
+#     b.append(1)
+#     b.append(2)
+#     print(b)
+# elif a[0]+a[2]==tar:
+#     b.append(0)
+#     b.append(2)
+#     print(b)
+# else:
+#     print(b)
+for i in range(0,len(a)):
+    for j in range(i+1,len(a)):
+        if a[i]+a[j]==tar:
+            b.append(i)
+            b.append(j)
+            print(b)
--- num.py
+++ num.py
@@ -32,18 +32,13 @@
 #     print(i*'*')
 
 
-# a=int(input("enter a number"))
-# for i in range(1,a+1):
-#       print('*'*i)
-# for i in range(a,0,-1):
-#       print('*'*i)
 
 
-# a=int(input("enter a number"))
-# for i in range(1,a+1):
-#   for j in range(1,i+1):  
-#     print(j,end='')
-#     print('')
+a=int(input("enter a number"))
+for i in range(1,a+1):
+  for j in range(1,i+1):  
+    print(j,end='')
+    print('')
 
 
 
