942db7b48fde63360dc9d88b8270219c3a27dfb7
amaljithkaliyodath <amaljithkaliyodath1@gmail.com>
2023-10-20 15:39:40 +0530

    add file

--- data.py
+++ data.py
@@ -1,25 +1,46 @@
 student={}
-teacher={}
-regno=1001
+regno=100
 studentli=[]
+teacher={}
+teacherlist=[]
+studentstore=[]
 while True:
     print('''
           1.student name
-          2.view
+          2.viewstudent
           3.teachers
-          4.exit''')
+          4.viewteachere
+          5.exit''')
     choice=int(input('enter your choice'))
     if choice==1:
         stdname=input('enter student name')
         student['stdname']=stdname
         stdage=int(input('enter student age'))
         student['stdage']=stdage
+        stddeppartment=input('enter student department')
+        student['stddepartment']=stddeppartment
         student['regno']=regno
         regno=regno+1
-        studentli.append(student.copy)
-    if choice==2:
-          print(studentli)
+        studentli.append(student.copy())
+    if choice==2:   
+              for i in studentli:
+                     for j,k in i.items():
+
+                        print(j,';',k)
     if choice==3:
          teachername=input('enter teacher name')
          teacher['teachername']=teachername
-        
+         teachdepartment=input('enter teaching department')
+         teacher['teacherdepartment']=teachdepartment
+         teacherlist.append(teacher.copy())
+    elif choice==4:    
+                  for i in teacherlist:
+                       for j,k in i.items():
+                           print(j,';',k)
+            
+    elif choice==5:  
+        stdname=input('enter student name')           
+        student['stdname']=stdname
+    elif choice==6:
+   
+           exit()
