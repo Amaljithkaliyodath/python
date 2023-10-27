daff78b1f401597a7bf10fc92eb5997e3bdf65ea
amaljithkaliyodath <amaljithkaliyodath1@gmail.com>
2023-10-27 15:48:25 +0530

    add file

--- data.py
+++ data.py
@@ -1,29 +1,31 @@
 student={}
 regno=100
 studentli=[]
-teachlist={}
+teachlist=[]
 teachstore=[]
 studentstore=[]
 while True:
     print('''
-          1.student name
-          2.viewstudent
-          3.teachers
+          1.student 
+          2.teachers
+          3.viewstudent
           4.viewteachere
-          5.exit''')
+          5.asign
+          6.viewdepartment
+          7.exit''')
     choice=int(input('enter your choice'))
     if choice==1:
-        stdname=input('enter student name')
-        student['stdname']=stdname
-        stdage=int(input('enter student age'))
-        student['stdage']=stdage
+        name=input('enter student name')
+        student['stdname']=name
+        age=int(input('enter student age'))
+        student['stdage']=age
         stddeppartment=input('enter student department')
         student['stddepartment']=stddeppartment
         student['regno']=regno
         regno=regno+1
         studentli.append(student.copy())
     if choice==2:
-             name=input('enter your name')         
+             name=input('enter teacher name')         
              teachlist.append(name)
         
     if choice==3:
@@ -33,14 +35,39 @@ while True:
     if choice==5: 
              student=input('enter student name') 
              for i in studentli:          
-                   studentstore.append(i["name"])
+                   studentstore.append(i['name'])
              b=len(teachlist)
              for j in range(b):
                     if student in studentstore:
                          for i in studentli:
-                               if i["name"]==student:
-                                i["teacher"]=teachlist[j]
-                    print(studentli)
+                               if i['name']==student:
+                                i['teachstore']=teachlist[j]
+                    # print(studentli)
+             for i in studentli:
+              print('-'*20)
+              for j,k in i.items():
+                  print(j,';',k)              
+              print('-'*20)
     if choice==6:
-   
+           student=input('enter department')
+           for i in studentli:
+                  studentstore.append(i['stddepartment'])
+
+
+           if student in studentstore:
+              for i in studentli: 
+
+                   if i['stddepartment']==student:
+                        for n in teachlist:
+
+                               i['teachstore']=teachlist[0]     
+                   else:
+                               i['teachstore']=teachlist[1]
+           for i in studentli:
+             print('-'*20)
+             for j,k in i.items():
+               print(j,';',k)              
+             print('-'*20)  
+
+    if choice==7:
            exit()
