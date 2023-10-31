4e1d061fc08e203365145c45a4c00e6d2cacd98a
amaljithkaliyodath <amaljithkaliyodath1@gmail.com>
2023-10-31 15:43:31 +0530

    add file

--- 17.py
+++ 17.py
@@ -1,10 +1,18 @@
 a={
-    "2":"abc",
-    "3":"def",
-    "4":"ghi",
-    "5":"jkl",
-    "6":"mno",
-    "7":"pqrs",
-    "8":"tuv",
-    "9":"wxyz"
+    2:"abc",
+    3:"def",
+    4:"ghi",
+    5:"jkl",
+    6:"mno",
+    7:"pqrs",
+    8:"tuv",
+    9:"wxyz"
 }
+b=int(input("enter a number"))
+c=b//10
+d=b%10
+num=a[c]
+num1=a[d]
+for i in num:
+    for j in num1:
+        print(i+j)
