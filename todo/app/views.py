05256c53d3c68e86d6eedd28d3a7dc9bdb4c2a66
amaljithkaliyodath <amaljithkaliyodath1@gmail.com>
2023-11-30 15:36:31 +0530

    add

--- todo/app/views.py
+++ todo/app/views.py
@@ -6,22 +6,29 @@ from .forms import *
 
 def index(request):
     context={}
-
-
-    if request.method=="POST":
-        if 'save' in request.POST:
-            todo_form=Todo_form(request.POST)
-            todo_form.save()
-
-
-
-        elif 'delete' in request.POST:
-          key=request.POST.get('delete')
-          todo_del=Todo.objects.get(id=key)
-          todo_del.delete()
-
     todo=Todo.objects.all()
     todo_form=Todo_form()
+    if request.method=="POST":
+        if 'save' in request.POST:
+           
+            key=request.POST.get('save')
+            if not key:
+                todo_form=Todo_form(request.POST)
+            else:
+                todo_edit=Todo.objects.get(id=key)
+                todo_form=Todo_form(request.POST,instance=todo_edit)
+
+        
+            todo_form.save()
+            todo_form=Todo_form()
+        elif 'delete' in request.POST:
+                    key=request.POST.get('delete')
+                    todo_del=Todo.objects.get(id=key)
+                    todo_del.delete()
+        elif 'edit' in request.POST:
+            key=request.POST.get('edit')
+            todo_edit=Todo.objects.get(id=key)
+            todo_form=Todo_form(instance=todo_edit)
 
     context['todo']=todo
     context['todo_form']=todo_form
